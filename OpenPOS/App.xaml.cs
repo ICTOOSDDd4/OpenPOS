1f12f6d7da7adeb0d89cb5bedc9ad95f6b7ac146
JMC-Cyber <jmccrans@gmail.com>
2022-11-13 00:23:31 +0100

    Project setup
    + Created .NET MAUI project
    + Created NUnit test project

---
+++ OpenPOS/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace OpenPOS;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ OpenPOS/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OpenPOS;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
