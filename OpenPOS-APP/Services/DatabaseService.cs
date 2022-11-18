e32b76391fed86e8a0a26779e87d4eabfc3f5c18
Tymen Vis <Tymenvis@outlook.com>
2022-11-18 14:59:39 +0100

    refactored dataabse code now you can use the db connection outside

--- OpenPOS-APP/Services/DatabaseService.cs
+++ OpenPOS-APP/Services/DatabaseService.cs
@@ -9,59 +9,24 @@ namespace OpenPOS_APP.Services
 {
     internal static class DatabaseService
     {
+        public static SqlConnection dbcontext { get; private set; }
         public static void Initialize()
         {
             OpenSqlConnection();
         }
-
         private static void OpenSqlConnection()
         {
             string connectionString = GetConnectionString();
 
-            using (SqlConnection connection = new SqlConnection())
+            using (dbcontext = new SqlConnection())
             {
-                connection.ConnectionString = connectionString;
+                dbcontext.ConnectionString = connectionString;
 
-                connection.Open();
-
-                String sql = "SELECT * FROM [dbo].[user]";
-
-                using (SqlCommand command = new SqlCommand(sql, connection))
-                {
-                    using (SqlDataReader reader = command.ExecuteReader())
-                    {
-                        System.Diagnostics.Debug.WriteLine(Environment.NewLine + "Retrieving data from database..." + Environment.NewLine);
-                        System.Diagnostics.Debug.WriteLine("Retrieved records:");
-
-                        //check if there are records
-                        if (reader.HasRows)
-                        {
-                            while (reader.Read())
-                            {
-                                string id = reader.GetInt32(0).ToString();
-                                string name = reader.GetString(1);
-                                string last_name = reader.GetString(2);
-
-                                //display retrieved record
-                                System.Diagnostics.Debug.WriteLine("{0},{1},{2}",id, name, last_name);
-                            }
-                        }
-                        else
-                        {
-                            System.Diagnostics.Debug.WriteLine("No data found.");
-                        }
-                    }
-                }
-                System.Diagnostics.Debug.WriteLine("State: {0}", connection.State);
-                System.Diagnostics.Debug.WriteLine("ConnectionString: {0}",
-                    connection.ConnectionString);
+                dbcontext.Open();
             }
         }
-
         static private string GetConnectionString()
         {
-            // To avoid storing the connection string in your code,
-            // you can retrieve it from a configuration file.
             return "";
         }
     }
