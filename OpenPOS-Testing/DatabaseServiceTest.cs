c43c3ec128b0f8ddd8290d1a627c2ccee8beef91
Tymen Vis <Tymenvis@outlook.com>
2022-11-19 15:51:52 +0100

    DatabaseTest File

---
+++ OpenPOS-Testing/DatabaseServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPOS_Testing
+{
+    public class DatabaseServiceTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            
+            Assert.Pass();
+        }
+    }
+}
