aad197b401b61d5a380219804ef7bd441a6672bd
JMC-Cyber <jmccrans@gmail.com>
2022-11-21 19:33:56 +0100

    Started on button checks

--- OpenPOS-APP/MainPage.xaml.cs
+++ OpenPOS-APP/MainPage.xaml.cs
@@ -1,29 +1,57 @@
 ﻿using Microsoft.Extensions.Configuration;
-
 namespace OpenPOS_APP;
 
 public partial class MainPage : ContentPage
 {
-	private int _count;
-	private IConfiguration _configuration;
+	private string _username;
+	private string _password;
+	private ResourceDictionary AppColors = new ResourceDictionary();
 	public MainPage(IConfiguration config)
 	{
 		InitializeComponent();
-		_configuration = config;
+		AppColors.Add("Resources/Styles/Colors.xaml", typeof(ResourceDictionary));
 	}
 
-	private async void OnCounterClicked(object sender, EventArgs e)
+	private void OnTextFilledUsername(object sender, EventArgs e)
 	{
-		_count++;
-
-		if (_count == 1)
-			CounterBtn.Text = $"Clicked {_count} time";
+		_username = nameof(UsernameEntry.Text);
+		if (string.IsNullOrEmpty(_username))
+		{
+			MainLoginButton.IsEnabled = false;
+			UsernameEntry.Placeholder = "Please enter your username";
+		} 
 		else
-			CounterBtn.Text = $"Clicked {_count} times";
+		{
+			MainLoginButton.IsEnabled = true;
+			if (AppColors.TryGetValue("OpenPos-Yellow", out var color))
+			{
+				MainLoginButton.BackgroundColor = (Color)color;
+			}
+		}
+	}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
+	private void OnTextFilledPassword(object sender, EventArgs e)
+	{
+		_password = nameof(PasswordEntry.Text);
+		if (string.IsNullOrEmpty(_password))
+		{
+			MainLoginButton.IsEnabled = false;
+			PasswordEntry.Placeholder = "Please enter your password";
+		}
+		else
+		{
+			MainLoginButton.IsEnabled = true;
+			if (AppColors.TryGetValue("OpenPos-Yellow", out var color))
+			{
+				MainLoginButton.BackgroundColor = (Color)color;
+			}
+		}
+	}
 
-   }
+	private void OnLoginButtonClicked(object sender, EventArgs e)
+	{
+		//TODO: Login auth with DB
+	}
 
     
 }
