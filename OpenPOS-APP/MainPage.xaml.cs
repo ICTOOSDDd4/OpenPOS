0336e4dfcbd78045fa46d7fc62653c20152e152a
Tymen Vis <Tymenvis@outlook.com>
2022-11-24 17:26:39 +0100

    removed unused code and changed entry to search

--- OpenPOS-APP/MainPage.xaml.cs
+++ OpenPOS-APP/MainPage.xaml.cs
@@ -26,63 +26,12 @@ public partial class MainPage : ContentPage
 	{
       await Shell.Current.GoToAsync(nameof(LoginScreen));
    }
-
-   private void ActivateButton(bool active)
-   {
-      if (active)
-      {
-         MainLoginButton.IsEnabled = true;
-         if (_appColors.TryGetValue("OpenPos-Yellow", out var color))
-         {
-            MainLoginButton.BackgroundColor = (Color)color;
-         }
-      }
-      else
-      {
-         MainLoginButton.IsEnabled = false;
-         if (_appColors.TryGetValue("Gray100", out var color))
-         {
-            MainLoginButton.BackgroundColor = (Color)color;
-         }
-         PasswordEntry.Placeholder = "Please enter your password";
-		}
-		else
-		{
-			MainLoginButton.IsEnabled = true;
-			if (_appColors.TryGetValue("OpenPos-Yellow", out var color))
-			{
-				MainLoginButton.BackgroundColor = (Color)color;
-			}
-		}
-	}
-
-	private async void OnLoginButtonClicked(object sender, EventArgs e)
-	{
-		//TODO: Login auth with DB
-		//await DisplayAlert("Test", "Logging in...", "OK");
-      if (UserAuth(_username, _password)) 
-		{
-			//TODO: ROUING
-		} else { await DisplayAlert("Oops", "You got it wrong", "Try again"); }
-		
-	}
 	
 	private async void CreateNewAccount_Tapped(object sender, EventArgs e)
 	{
 		// Navigation.PushAsync();
 		await DisplayAlert("Test", "Creating account...", "OK");
 
-	}
-
-	private bool UserAuth(string username, string password)
-	{
-		User user = UserService.Authenticate(_username, _password);
-
-		if (user != null)
-		{
-			ApplicationSettings.LoggedinUser = user;
-			return true;
-		} else { return false; }
 	}
     private void OnSearch(object sender, TextChangedEventArgs e)
     {
