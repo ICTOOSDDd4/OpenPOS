0a40485dfb6b638635bfea5a9dab221c9a85adc3
JMC-Cyber <jmccrans@gmail.com>
2022-11-24 17:03:14 +0100

    Added ProductViews

--- OpenPOS-APP/MenuPage.xaml.cs
+++ OpenPOS-APP/MenuPage.xaml.cs
@@ -7,10 +7,12 @@ public partial class MenuPage : ContentPage
 {
 	public List<Product> Products { get; set; }
 	public List<Product> SelectedProducts { get; set; }
+	private HorizontalStackLayout HorizontalLayout;
 	public MenuPage()
 	{
 		Products = ProductService.GetAll();
 		InitializeComponent();
+		SelectedProducts = new List<Product>();
 
 		for (int i = 0; i < Products.Count; i++)
 		{
@@ -21,10 +23,23 @@ public partial class MenuPage : ContentPage
 
 	public void AddProductToLayout(Product product)
 	{
-		ProductView productView = new ProductView();
+      if (HorizontalLayout == null || HorizontalLayout.Children.Count % 8 == 0)
+      {
+			AddHorizontalLayout();
+      }
+      ProductView productView = new ProductView();
 		productView.SetProductValues(this,product);
-		hLayout.Add(productView);
+      HorizontalLayout.Add(productView);
 	}
+
+	private void AddHorizontalLayout()
+	{
+      HorizontalStackLayout hLayout = new HorizontalStackLayout();
+		hLayout.Spacing = 20;
+		hLayout.Margin = new Thickness(10);
+		MainVerticalLayout.Add(hLayout);
+		HorizontalLayout = hLayout;
+   }
 	
 	
 	
