e16cf385bc414788dcea8a24ec732bcbdf170439
JMC-Cyber <jmccrans@gmail.com>
2022-11-24 15:09:40 +0100

    Finished product Item cardview

--- OpenPOS-APP/ProductView.xaml.cs
+++ OpenPOS-APP/ProductView.xaml.cs
@@ -4,18 +4,55 @@ namespace OpenPOS_APP;
 
 public partial class ProductView : ContentView
 {
-	public ProductView()
-	{
-		InitializeComponent();
+   public int Amount { get; set; }
 
+
+   public ProductView()
+   {
+      InitializeComponent();
       MainVerticalLayout.Shadow = new Shadow
       {
          Offset = new Point(10, 10),
          //Radius = 10,
          Brush = Brush.Black,
          Opacity = 0.12f,
-       };
-
-      MainVerticalLayout.BackgroundColor = Color.FromRgb(360, 360, 360);
+      };
    }
+
+   public void SetPoductValues(string name, string desc, string imagePath)
+   {
+      ProductName.Text = name;
+      ProductInfo.Text = desc;
+      ProductImage.Source = imagePath;
+   }
+
+   private void OnClickedToevoegen(object sender, EventArgs e)
+   {
+      Amount++;
+      AmountCount.Text = Amount.ToString();
+   }
+
+   private void OnClickedVerwijderen(object sender, EventArgs e)
+   {
+      Amount--;
+      AmountCount.Text = Amount.ToString();
+   }
+
+    private void AmountCount_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+      if (AmountLabel != null)
+      {
+         if (Amount > 0)
+         {
+            AmountLabel.IsVisible = true;
+            DeleteButton.IsVisible = true;
+         }
+         else
+         {
+            AmountLabel.IsVisible = false;
+            DeleteButton.IsVisible = false;
+         }
+      }
+      
+    }
 }
