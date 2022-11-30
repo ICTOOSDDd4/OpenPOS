3290f8029f3ed6f288ea6a2b99a18f2b470b29ab
Tymen Vis <Tymenvis@outlook.com>
2022-11-30 15:18:43 +0100

    Roleinterface

---
+++ OpenPOS-APP/Services/Interfaces/IRoleService.cs
@@ -0,0 +1,14 @@
+﻿using OpenPOS_APP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenPOS_APP.Services.Interfaces
+{
+    internal interface IRoleService
+    {
+        public bool IsAuthorized(User user, Role role);
+    }
+}
