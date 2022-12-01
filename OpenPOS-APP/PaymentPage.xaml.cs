64aaeeb6f8900a83e8ae73dfd268baa67011b1b0
Jort <39172426+jortdr@users.noreply.github.com>
2022-12-01 15:40:24 +0100

    temporary push

--- OpenPOS-APP/PaymentPage.xaml.cs
+++ OpenPOS-APP/PaymentPage.xaml.cs
@@ -1,15 +1,41 @@
 ﻿using System.Net;
+using OpenPOS_APP.Models;
 using OpenPOS_APP.Services;
+using OpenPOS_APP.Services.Models;
 using OpenPOS_APP.Settings;
 
 namespace OpenPOS_APP;
 
 public partial class PaymentPage : ContentPage
 {
+	public static Transaction CurrentTransaction { get; set; }
+	public static int RequiredPayments { get; set; }
+	
+	private int CurrentlyPaid { get; set; }
 	public PaymentPage()
 	{
 		InitializeComponent();
 		
 		QRCode.Source = UtilityService.GenerateQrCodeFromUrl("https://www.google.com"); //TODO: PLACE TIKKIE URL HERE
+	}
+
+	public static void SetTransaction(Transaction transaction, int numberOfRequiredPayments)
+	{
+		CurrentTransaction = transaction;
+		RequiredPayments = numberOfRequiredPayments;
+	}
+
+	private async void OnPaymentStatusCheck_Clicked(object sender, EventArgs e)
+	{ 
+		bool status = IsPaymentComplete();
+	}
+	
+	private bool IsPaymentComplete()
+	{
+		var transactionInformation = TikkiePayementService.GetTransactionInformation(CurrentTransaction.PaymentRequestToken);
+		
+		CurrentlyPaid = transactionInformation.NumberOfPayments;
+		
+		return transactionInformation.NumberOfPayments >= RequiredPayments;
 	}
 }
