52e966e3c2a111fe9bc8da4fec545a52e1219cd9
TinyTani <guus1207@live.nl>
2022-12-05 11:45:10 +0100

    fixed Testing

--- OpenPOS-Testing/OrderLineModelTest.cs
+++ OpenPOS-Testing/OrderLineModelTest.cs
@@ -11,13 +11,15 @@ public class OrderLineModelTest
     {
         int Order_Id = 1;
         int Product_Id = 3;
+        bool Status = false;
         int Amount = 5;
         string Comment = "Test";
         
-        OrderLine orderLine = new OrderLine(Order_Id, Product_Id, Amount, Comment);
+        OrderLine orderLine = new OrderLine(Order_Id, Product_Id, Status, Amount, Comment);
         
         Assert.AreEqual(Order_Id, orderLine.Order_id);
         Assert.AreEqual(Product_Id, orderLine.Product_id);
+        Assert.AreEqual(Status, orderLine.Status);
         Assert.AreEqual(Amount, orderLine.Amount);
         Assert.AreEqual(Comment, orderLine.Comment);
         
