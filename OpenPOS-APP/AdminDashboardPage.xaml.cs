f4ba5b1cd6a62ff834dd0586973af5aea70d94d9
JMC-Cyber <jmccrans@gmail.com>
2022-12-19 12:39:33 +0100

    Unpushed fixes

--- OpenPOS-APP/AdminDashboardPage.xaml.cs
+++ OpenPOS-APP/AdminDashboardPage.xaml.cs
@@ -8,37 +8,13 @@ namespace OpenPOS_APP;
 
 public partial class AdminDashboardPage : ContentPage
 {
-	public ISeries[] RevenueSeries { get; set; }
-	public string RevenueTitle { get; set; }
 	private OrderController _orderController;
 	public AdminDashboardPage()
 	{
 		_orderController = new OrderController();
-		RevenueTitle = "Revenue";
 		InitializeComponent();
-		//AdminHeader.CurrentPage = this;
-		CreateRevueneGraph();
+		AdminHeader.CurrentPage = this;
 	}
 
-	private void CreateRevueneGraph()
-	{
-		// List<OrderLine> lines = _orderController.GetOrderLines();
-		// foreach (OrderLine line in lines)
-		// {
-		// 	IN DEVELOPMENT
-		// }
-		RevenueSeries = new ISeries[]
-		{ // Dev test data
-			new LineSeries<double>
-			{
-				Values = new List<double> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 },
-				Name = "Revenue",
-				Stroke = new SolidColorPaint(SKColors.Blue) { StrokeThickness = 4 },
-				Fill = null,
-				GeometryFill = null,
-				GeometryStroke = null
-			}
-		};
-		RevenueChart.Series = RevenueSeries;
-	}
+	
 }
