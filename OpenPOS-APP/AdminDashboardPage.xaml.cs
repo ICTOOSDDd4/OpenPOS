e9be3187202fd14983c07090cfbbb8a0385c69d2
JMC-Cyber <jmccrans@gmail.com>
2023-01-07 23:19:51 +0100

    Added some stuff to the dashboard

--- OpenPOS-APP/AdminDashboardPage.xaml.cs
+++ OpenPOS-APP/AdminDashboardPage.xaml.cs
@@ -1,8 +1,14 @@
+using OpenPOS_Controllers;
+using OpenPOS_Database;
+using OpenPOS_Models;
+
 namespace OpenPOS_APP;
 
 public partial class AdminDashboardPage : ContentPage
 {
 	private bool _isInitialized;
+	private ProductController _productController;
+	private CategoryController _categoryController;
 	public AdminDashboardPage()
 	{
 		InitializeComponent();
@@ -11,26 +17,49 @@ public partial class AdminDashboardPage : ContentPage
 	
 	protected override void OnSizeAllocated(double width, double height)
 	{
+		double margin = VerticalStackLayout.Spacing * 3;
 		// Gets called by MAUI
 		base.OnSizeAllocated(width, height);
 		if (!_isInitialized)
 		{
 			_isInitialized = true;
-			double margin = VerticalStackLayout.Spacing * 3;
-			
 			double itemWidth = (width / 2) - margin;
-			RChart.WidthRequest = itemWidth;
-			OChart.WidthRequest = itemWidth;
-			
-
 			double itemHeight = (height / 2) - margin;
-			RChart.HeightRequest = itemHeight;
-			OChart.HeightRequest = itemHeight;
-			
-			string revenueValue = String.Format(((Math.Round(RChart.TotalPrice) ==RChart.TotalPrice) ? "{0:0}" : "{0:0.00}"), RChart.TotalPrice);
-			TotalRevenueLabel.Text = $"Total Revenue: € {revenueValue}";
-			TotalOrderLabel.Text = "Total Amount of Orders: " + OChart.TotalAmount;
+			SetGraphs(itemWidth, itemHeight);
+			SetLists(itemWidth, itemHeight);
 		}
+	}
+	
+	private void SetGraphs(double itemWidth, double itemHeight)
+	{
+			
+		RChart.WidthRequest = itemWidth;
+		OChart.WidthRequest = itemWidth;
+
+		RChart.HeightRequest = itemHeight;
+		OChart.HeightRequest = itemHeight;
+			
+		string revenueValue = String.Format(((Math.Round(RChart.TotalPrice) == RChart.TotalPrice) ? "{0:0}" : "{0:0.00}"), RChart.TotalPrice);
+		TotalRevenueLabel.Text = $"Total Revenue: € {revenueValue}";
+		TotalOrderLabel.Text = "Total Amount of Orders: " + OChart.TotalAmount;
+	}
+
+	private void SetLists(double itemWidth, double itemHeight)
+	{
+		double width = itemWidth / 2;
+		TopCategories.WidthRequest = width;
+		TopProducts.WidthRequest = width;
+		BottomCategories.WidthRequest = width;
+		BottomProducts.WidthRequest = width;
+		
+		TopCategories.HeightRequest = itemHeight;
+		TopProducts.HeightRequest = itemHeight;
+		BottomCategories.HeightRequest = itemHeight;
+		BottomProducts.HeightRequest = itemHeight;
+
+		// List<Product> products = _productController.GetAllProducts();
+		// List<Category> categories = _categoryController.GetAll();
+		
 	}
 
 	
